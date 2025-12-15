e2d29c9196131d8ffce9b6013cfb8346e127e2d5
7li7hmed0-sudo <7li.7hmed0@gmail.com>
2025-12-15 18:01:40 +0300

    Updat.lua

--- Untitled-1.lua
+++ Untitled-1.lua
@@ -1,3 +1,4 @@
+print("Loaded from GitHub")
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
@@ -193,3 +194,4 @@ SaveManager:BuildConfigSection(Tabs.Settings)
 Window:SelectTab(1)
 
 SaveManager:LoadAutoloadConfig()
+
